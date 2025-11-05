3b9a892d11a8c0875c4f679cc2b5fe8c800b4a07
Scorlines <fadhelf694@gmail.com>
2025-11-05 11:49:01 +0700

    fix error

--- nuve-fe/src/App.jsx
+++ nuve-fe/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, Men, Women } from './components';
+import { Home, Man, Woman } from './components';
 import './style.css';
 
 function App() {
