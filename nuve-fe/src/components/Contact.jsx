cefe6c3b517d2f0758ce88365bd188eac8fb48db
Rizpat <mac@MacBook-Air-mac-2.local>
2025-11-09 04:30:45 +0700

    Perbaikan pada halaman contact

--- nuve-fe/src/components/Contact.jsx
+++ nuve-fe/src/components/Contact.jsx
@@ -31,7 +31,7 @@ export default function Contact() {
         <div className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/about">About Us</Link>
-          <a href="#product">Product</a>
+          <Link to="/product">Product</Link>
 
           <div className="nav-dropdown">
             <a
